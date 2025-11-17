c5683ce2d38f1703ebd62d2bea93f44adfd3830e
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-16 17:44:12 -0800

    Initial Commit

---
+++ stanfordCourseSwiftUI/stanfordCourseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanfordCourseSwiftUIApp.swift
+//  stanfordCourseSwiftUI
+//
+//  Created by Максим Минаков on 16.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct stanfordCourseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
